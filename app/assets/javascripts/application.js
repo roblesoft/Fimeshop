78c87f60f6980f01dab3473d40d055404b1e5fb9
Uriel R <uriel_r@outlook.com>
2018-04-10 10:11:51 -0500

    navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require popper.min
 //= require bootstrap.min
-//= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
