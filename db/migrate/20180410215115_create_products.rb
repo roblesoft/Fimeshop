3e3f401c448ee63188cbdf62bc3ad326b48065b6
Uriel R <uriel_r@outlook.com>
2018-04-16 18:50:48 -0500

    error al subir a heroku

--- db/migrate/20180410215115_create_products.rb
+++ db/migrate/20180410215115_create_products.rb
@@ -4,13 +4,6 @@ class CreateProducts < ActiveRecord::Migration[5.0]
       t.string :name
       t.text :description
       t.float :price
-      t.boolean :available
-      t.boolean :desktop
-      t.boolean :clothing
-      t.boolean :accessories
-      t.boolean :thermos
-      t.boolean :others
-
       t.timestamps
     end
   end
--- db/migrate/20180412234146_remove_values_to_products.rb
+++
@@ -1,9 +0,0 @@
-class RemoveValuesToProducts < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :products, :desktop, :boolean
-    remove_column :products, :clothing, :boolean
-    remove_column :products, :accesories, :boolean
-    remove_column :products, :thermos, :boolean
-    remove_column :products, :other, :boolean
-  end
-end
